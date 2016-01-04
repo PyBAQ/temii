7dcddbee5886530fca069d958410493c6160e538
leonardoo <lorozco@technisupport.com>
2016-01-04 14:34:12 -0500

    convirtiendo tabs a espacios, dando portando modelos con soporte python2/3 y usando managers en charlas

--- apps/votos/models.py
+++ apps/votos/models.py
@@ -1,43 +1,49 @@
 #encoding:utf-8
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 
-ESTADO_CHOICES = (
-    			   ('posible','Temas Posibles'),
-    			   ('agendado','Temas agendados'),
-    			   ('finalizado','Temas finalizados'),)
+from . import constants
+
+from .managers import charla
 
 
+@python_2_unicode_compatible
 class Categoria(models.Model):
-	nombre = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255)
 
-	def __unicode__(self):
-		return self.nombre
+    def __str__(self):
+        return self.nombre
 
 
-# Create your models here.
+@python_2_unicode_compatible
 class Charla(models.Model):
-	titulo = models.CharField(max_length=255)
-	descripcion = models.TextField(blank=True, null=True)
-	fecha_publicacion = models.DateTimeField(auto_now_add=True)
-	categorias = models.ManyToManyField(Categoria)
-	prerequisitos = models.TextField(blank=True, null=True)
-	estado = models.CharField(max_length=255, choices=ESTADO_CHOICES, default="posible")	
-	fecha_taller = models.DateField(blank=True,null=True)
-	votos = models.PositiveIntegerField(default=0)
-	usuario = models.ForeignKey(User)
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    categorias = models.ManyToManyField(Categoria)
+    prerequisitos = models.TextField(blank=True, null=True)
+    estado = models.CharField(max_length=255, choices=constants.ESTADO_CHOICES, default="posible")
+    fecha_taller = models.DateField(blank=True,null=True)
+    votos = models.PositiveIntegerField(default=0)
+    usuario = models.ForeignKey(User)
 
-	class Meta:
-		ordering = ['votos']
+    objects = models.Manager()
+    posibles = charla.CharlaPosibleManager()
+    agendadas = charla.CharlaAgendadaManager()
+    finalizadas = charla.CharlaFinalizadaManager()
 
-	def __unicode__(self):
-		return self.titulo
+    class Meta:
+        ordering = ['votos']
+
+    def __str__(self):
+        return self.titulo
 
 
 class Voto(models.Model):
-	usuario = models.ForeignKey(User)
-	charla = models.ForeignKey(Charla)
-	timestamp = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User)
+    charla = models.ForeignKey(Charla)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		unique_together = ('usuario', 'charla')
+    class Meta:
+        unique_together = ('usuario', 'charla')
