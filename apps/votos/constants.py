f6809455a9ea1f4dcd0ffe407640438650763917
leonardoo <lorozco@technisupport.com>
2016-01-04 14:31:55 -0500

    convirtiendo opciones de estado en constantes

---
+++ apps/votos/constants.py
@@ -0,0 +1,9 @@
+ESTADO_POSIBLE = 'posible'
+ESTADO_AGENDADO = 'agendado'
+ESTAOO_FINALIZADO = 'finalizado'
+
+ESTADO_CHOICES = (
+    			  (ESTADO_POSIBLE, 'Temas Posibles'),
+    			  (ESTADO_AGENDADO, 'Temas agendados'),
+    			  (ESTAOO_FINALIZADO, 'Temas finalizados'),
+                 )
