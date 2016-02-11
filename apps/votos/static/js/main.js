e778c8863e68c2655651aeb594de8e82dd0e1fc0
Sergio C. Orozco Torres <scot3004@gmail.com>
2016-02-11 13:33:55 +0000

    Eliminados scripts de registrar_charla.html pasados a main.js

--- apps/votos/static/js/main.js
+++ apps/votos/static/js/main.js
@@ -1,1 +1,6 @@
 $(".button-collapse").sideNav();
+
+ $(document).ready(function() {
+  $('.material-form select').material_select();
+  $('.material-form textarea').addClass('materialize-textarea');
+});
