dbd50c0f5d12916c3a14e67e38b51453c8aa4fde
Cesar Augusto <bash5426@gmail.com>
2016-03-19 07:23:23 -0500

    ajustando base y production

--- votos/settings/base.py
+++ votos/settings/base.py
@@ -125,14 +125,4 @@ SOCIAL_AUTH_GITHUB_KEY = '12345llavedeprueba'
 SOCIAL_AUTH_GITHUB_SECRET = '12345678901llavedeprueba'
 
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = "/"
-
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'),)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
