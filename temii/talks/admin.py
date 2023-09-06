190f98882fe498e8184824eef9acfb201cc7faf0
Sergio C. Orozco Torres <scot3004@gmail.com>
2023-09-05 21:17:15 -0500

    Enums Choices en los talks

--- temii/talks/admin.py
+++ temii/talks/admin.py
@@ -1,21 +1,9 @@
 from django.contrib import admin
 
-from .models import Talk, TalkLanguage, TalkLevel
+from .models import Talk
 
 
 @admin.register(Talk)
 class TalkAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    search_fields = ["name"]
-
-
-@admin.register(TalkLanguage)
-class TalkLanguageAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    search_fields = ["name"]
-
-
-@admin.register(TalkLevel)
-class TalkLevelAdmin(admin.ModelAdmin):
     list_display = ["name"]
     search_fields = ["name"]
--- temii/talks/models.py
+++ temii/talks/models.py
@@ -1,27 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from temii.users.models import User
 
 
-class TalkLanguage(models.Model):
-    name = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.name
-
-
-class TalkLevel(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
-
-
 class Talk(models.Model):
+    class Level(models.IntegerChoices):
+        BEGINNER = 1, _("Beginner")
+        INTERMEDIATE = 2, _("Intermediate")
+        ADVANCED = 3, _("Advanced")
+
+    class Language(models.TextChoices):
+        ES = "es", _("Spanish")
+        EN = "en", _("English")
+
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=60)
     description = models.CharField(max_length=300)
-    level = models.ForeignKey(TalkLevel, on_delete=models.PROTECT)
-    language = models.ForeignKey(TalkLanguage, on_delete=models.PROTECT)
+    level = models.PositiveIntegerField(choices=Level.choices, default=Level.BEGINNER)
+    language = models.CharField(max_length=2, choices=Language.choices, default=Language.ES)
     timezone = models.CharField(max_length=60)
     comments = models.CharField(max_length=300)
